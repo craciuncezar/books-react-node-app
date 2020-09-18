ac754361fcc2bbf073ca6bdce2104eb6169ad0a5
Craciun Cezar <craciuncezar1996@gmail.com>
2020-09-18 22:32:25 +0300

    Use hooks in app and add typescript dep

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,32 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import Home from "./modules/home";
 import LandPage from "./modules/landpage";
 import { setTokenFromStorage } from "./redux/user/user.actions";
 
-class App extends Component {
-  componentDidMount() {
+const App = ({ user, setToken }) => {
+  useEffect(() => {
     const token = window.localStorage.getItem("jwt-token") || "";
     if (token) {
-      this.props.setToken(token);
+      setToken(token);
     }
-  }
+  }, [setToken]);
 
-  render() {
-    if (this.props.user.token === "") {
-      return <LandPage />;
-    } else {
-      return <Home />;
-    }
+  if (user.token === "") {
+    return <LandPage />;
+  } else {
+    return <Home />;
   }
-}
+};
 
 function mapStateToProps(state) {
   return {
-    user: state.user
+    user: state.user,
   };
 }
 
 const mapDispatchToProps = {
-  setToken: setTokenFromStorage
+  setToken: setTokenFromStorage,
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
